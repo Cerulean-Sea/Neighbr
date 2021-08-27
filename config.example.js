39ec64fe923ca21316f40877f51bfeace1ac4086
Christian Stutzman <christian.stutzman01@gmail.com>
2021-08-27 14:29:29 -0700

    implemented helper-functions folder with renderMap function inside meant to render dynamic maps from the Google Maps Embed API s iframes

---
+++ config.example.js
@@ -0,0 +1,7 @@
+const googleMapsApiKey = 'YOUR_GOOGLE_MAPS_API_KEY_HERE';
+// note: API key must be from the Google Embed API, found here:
+// https://developers.google.com/maps/documentation/embed/get-started
+
+module.exports = {
+  googleMapsApiKey
+}
