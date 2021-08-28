bae459f1057db9f741d28a4d761f862660b8ee34
Ben Bernardy <benbernardy@gmail.com>
2021-08-28 09:26:37 -0700

    ADDED: basic setup for recieving req at signin endpoint

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,7 +1,13 @@
 const User = require('../database/User');
 
 const signin = (req, res) => {
-
+  try {
+    const { email, pass } = req.body;
+    res.set(200).send("Recieved creds on req.body")
+  }
+  catch (error) {
+    res.set(400).send(error)
+  }
 };
 
 const signup = (req, res) => {
