5283f30050e1095909c8a087ef5fbb679e6aa561
Andy Vo <andyqvo@gmail.com>
2021-08-28 16:11:23 -0700

    Added API routes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,5 +9,21 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+// Posts
 export const getPosts = () => API.get(`/posts`);
+export const getPostById = (postId) => API.get(`/posts/${postId}`);
+export const getPostsByUserId = (userId) => API.get(`/posts/users/${userId}`);
+export const postPost = (post) => API.post(`/posts/create`, post);
+export const updatePost = (postId, post) => API.patch(`/posts/${postId}`, post);
+export const deletePost = (postId) => API.delete(`/posts/${postId}`);
+
+// Comments
+export const getComments = () => API.get(`/comments`);
+export const getCommentById = (commentId) => API.get(`/comments/${commentId}`);
+export const getCommentsByUserId = (userId) => API.get(`/comments/users/${userId}`);
+export const postComment = (comment) => API.post(`/comments/create`, comment);
+export const updateComment = (commentId, comment) => API.patch(`/comments/${commentId}`, comment);
+export const deleteComment = (commentId) => API.delete(`/comments/${commentId}`);
+
+// Users
 export const signUp = (formData) => API.post(`/users/signup`, formData);
