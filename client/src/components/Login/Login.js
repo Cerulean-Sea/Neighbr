33929c5076bce7b1ccff840f991b071d7eeee5bb
Ben Bernardy <benbernardy@gmail.com>
2021-08-28 18:11:54 -0700

    CHANGED: Refactored to no modal, single form. NEEDS: styling and redux. OAuth options too

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,18 +7,35 @@ import {
 import { LockOutlined } from '@material-ui/icons'
 import axios from 'axios';
 import useStyles from './styleLogin';
-import SignUp from '../Signup/SignUp';
 
 export default function Login() {
   const classes = useStyles();
-  const [ email, setEmail ] = useState('');
-  const [ pass, setPass ] = useState('');
-
-  const handleSignIn = () => {
-    axios.post('/api/users/signin',{ email, pass })
-    .then(res => res)
-    .catch(err => console.error(err))
+  const initialState = {
+    firstName : '',
+    lastName : '',
+    email : '',
+    password : '',
+    passwordConfirm : '',
+    zipcode : '',
   }
+  const [ formData, setFormData ] = useState(initialState);
+  const [ isSignUp, setIsSignUp ] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      // need to match to textfield names
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const endpoint = isSignUp ? 'signup' : 'signin';
+    axios.post(`/api/users/${endpoint}`, formData)
+    .then(res => res)
+    .catch(err => err)
+    }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -30,12 +47,39 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form
-          className={classes.form}
-            >
+        <form className={classes.form} onSubmit={handleSubmit}>
+          {isSignUp &&
+          <Grid container spacing={2}>
+            <TextField
+            onChange={(e) => { handleChange(e) }}
+            value={formData.firstName}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            name="firstName"
+            autoComplete="given-name"
+            autoFocus
+          />
           <TextField
-            onChange={(e) => setEmail( e.target.value ) }
-            value={email}
+            onChange={(e) => { handleChange(e) }}
+            value={formData.lastName}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="family-name"
+            autoFocus
+          />
+          </Grid>}
+          <TextField
+            onChange={(e) => { handleChange(e) }}
+            value={formData.email}
             variant="standard"
             margin="normal"
             required
@@ -47,40 +91,64 @@ export default function Login() {
             autoFocus
           />
           <TextField
-            onChange={(e) => setPass( e.target.value ) }
-            value={pass}
+            onChange={(e) => { handleChange(e) }}
+            value={formData.password}
             variant="standard"
             margin="normal"
             required
             fullWidth
-            name="password"
-            label="Password"
-            type="password"
             id="password"
-            autoComplete="current-password"
+            label="Password"
+            name="password"
+            autoComplete="password"
+            autoFocus
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+          {isSignUp &&
+          <>
+          <TextField
+            onChange={(e) => { handleChange(e) }}
+            value={formData.passwordConfirm}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            id="passwordConfirm"
+            label="Confirm Password"
+            name="passwordConfirm"
+            autoComplete="password"
+            autoFocus
           />
+
+          <TextField
+            onChange={(e) => { handleChange(e) }}
+            value={formData.zip}
+            variant="standard"
+            margin="normal"
+            required
+            fullWidth
+            id="zipcode"
+            label="Zip Code"
+            name="zipcode"
+            autoComplete="postal-code"
+            autoFocus
+          />
+          </>
+          }
           <Button
-            type="input"
-            onClick={handleSignIn}
+            type="submit"
+            onClick={handleSubmit}
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            {isSignUp ? "Sign Up!" : "Sign In"}
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
             <Grid item>
-              <SignUp />
+              <Button onClick={()=>{setIsSignUp(!isSignUp)}}>
+                No Account? Sign Up!
+              </Button>
             </Grid>
           </Grid>
         </form>
