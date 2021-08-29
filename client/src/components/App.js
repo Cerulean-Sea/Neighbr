4c5450934d8f09ebd6659b8c26f62014ae71e8b2
Andy Vo <andyqvo@gmail.com>
2021-08-29 09:18:25 -0700

    Update App.js
    fixed merge conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import Login from './Login/Login.js';
-=======
 import renderMap from '../helper-functions/renderMap';
 import Login from './Login/Login';
->>>>>>> main
 
 const App = () => {
 
   return (
-<<<<<<< HEAD
-    <Login />
-=======
     <div className="app">
       App
       <Login/>
     </div>
->>>>>>> main
   )
 }
 
