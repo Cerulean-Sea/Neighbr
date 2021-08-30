4a2706e2329366d64703fe74166a377627322494
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-08-30 09:40:45 -0700

    Implemented image preview for website links

--- client/src/components/PostForm/sub-components/ThumbnailItem.jsx
+++ client/src/components/PostForm/sub-components/ThumbnailItem.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 var ThumbnailItem = (props) => (
     <div>
-        <p>{props.item}</p>
+        {/* <p>{props.item}</p> */}
+        <img src={props.item} />
         <button value={props.item} onClick={props.removePhoto}>Remove Image</button>
     </div>
 )
