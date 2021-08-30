92e8d16502c14b40bcac9aca9f13d5427e538f1e
Andy Vo <andyqvo@gmail.com>
2021-08-30 14:05:05 -0700

    Added photos to schema

--- server/database/Post.js
+++ server/database/Post.js
@@ -23,7 +23,7 @@ const postSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Comment'
   }],
-  photos: []
+  photos: [String]
 });
 
 const Post = mongoose.model('Post', postSchema);
