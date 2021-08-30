31bd927c702739ecf024de5d6d680d24eb2fe7a1
Ben Bernardy <benbernardy@gmail.com>
2021-08-30 12:36:27 -0700

    ADDED: Theme provider to App. CREATED: themeApp.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,26 +5,32 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { ThemeProvider } from '@material-ui/core';
+
 import renderMap from '../helper-functions/renderMap';
 import PostForm from './PostForm/PostForm.jsx';
 import Login from './Login/Login';
-import { useSelector } from 'react-redux';
+import mainTheme from './ThemeApp';
 
 const App = () => {
 
 const AUTH = useSelector(state => state.firebase);
 
   return (
-    <Router>
-      <div className="app">
-        <Switch>
-          <Route exact path="/">
-            {!AUTH && <Login />}
-          </Route>
-        </Switch>
-        <PostForm />
-      </div>
-    </Router>
+    <ThemeProvider theme={}>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route exact path="/">
+              {!AUTH && <Login />}
+              {AUTH && <div> This content after credentials are verified</div>}
+            </Route>
+          </Switch>
+          <PostForm />
+        </div>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/components/ThemeApp.js
@@ -0,0 +1,5 @@
+import { creatueMuiTheme } from '@material-ui/core';
+
+export default mainTheme = creatueMuiTheme({
+
+});
