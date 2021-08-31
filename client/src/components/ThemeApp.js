d94c5ba2ecefc0869e7c407ddb95d8750fe4564e
Ben Bernardy <benbernardy@gmail.com>
2021-08-31 15:27:06 -0700

    ADDED: Basic MUI theme

--- client/src/components/ThemeApp.js
+++ client/src/components/ThemeApp.js
@@ -1,5 +1,51 @@
-import { creatueMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
 
-export default mainTheme = creatueMuiTheme({
+const clientColors = {
+  blue: '#457F96',
+  teal: '#32BCAC',
+  yellow: '#E9C46A',
+  orange: '#F4A261',
+  red: '#E2522E',
 
+}
+const mainTheme = createTheme({
+  palette: {
+    common: {
+      black: '#05090A'
+    },
+    primary: {
+      main: clientColors.blue
+    },
+    secondary: {
+      main: clientColors.teal
+    },
+    error: {
+      main: clientColors.red
+    },
+    warning: {
+      main: clientColors.orange
+    },
+    info: {
+      main: '#FAD1B0'
+    },
+  },
+  background: {
+    paper: '#ffffff',
+    default: '#FCF8EC'
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global' : {
+        html : {
+          backgroundImage: 'url("./assets/landing-page.jpg")',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }
+      }
+    },
+  }
 });
+
+export default mainTheme;
