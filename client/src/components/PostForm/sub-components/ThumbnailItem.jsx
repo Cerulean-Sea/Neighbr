cf7fba8b51d842708344759815d08a5a36b8bf91
Jean-Luc Felix <jlfelix522@yahoo.com>
2021-08-30 19:03:55 -0700

    Reverted back to regular button tag in ThumbnailItem

--- client/src/components/PostForm/sub-components/ThumbnailItem.jsx
+++ client/src/components/PostForm/sub-components/ThumbnailItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 var ThumbnailItem = (props) => (
     <div>
-        {/* <p>{props.item}</p> */}
         <img src={props.item} />
         <button value={props.item} onClick={props.removePhoto}>Remove Image</button>
     </div>
 )
 
 export default ThumbnailItem;
+
+
+// var ThumbnailItem = (props) => (
+//     <div>
+//         {/* <p>{props.item}</p> */}
+//         <img src={props.item} />
+//         <button value={props.item} onClick={props.removePhoto}>Remove Image</button>
+//     </div>
+// )
