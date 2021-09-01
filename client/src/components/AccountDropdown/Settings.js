c82fb066303c82d2b3a827bd03af6dc349d590c0
Anna <a.solilee@gmail.com>
2021-09-01 10:44:32 -0700

    Work in progress

--- client/src/components/AccountDropdown/Settings.js
+++ client/src/components/AccountDropdown/Settings.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, RaisedButton } from '@material-ui/core';
+import { TextField, RaisedButton, withStyles, InputBase, FormControl, InputLabel } from '@material-ui/core';
 
 function Settings() {
   const state = {
@@ -9,33 +9,70 @@ function Settings() {
     zipcode : '',
   }
 
-
   const AUTH = JSON.parse(localStorage.getItem('profile'))
-  // const [ formInput, changeFormInput ] = useState(state);
 
-  // const handleChange = (e) => {
-  //   changeFormInput({
-  //     [e.target.name]: e.target.value
-  //   })
-  // }
-  // const { firstName, lastName, email, zipcode } = this.state;
-  // const values = { firstName, lastName, email, zipcode };
-
+  const BootstrapInput = withStyles((theme) => ({
+    root: {
+      'label + &': {
+        marginTop: theme.spacing(3),
+      },
+    },
+    input: {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.common.white,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      width: 'auto',
+      padding: '10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  }))(InputBase);
 
   return (
     <div>
       <h1>Edit Account Information</h1>
       <h2>Account Information</h2>
-      {/* <div
-        handleChange={this.handleChange}
-        values={values}
-      > */}
+
+      <form className={classes.root} noValidate autoComplete="off">
         <TextField
         hintText="First Name *:"
         floatingLabelText={AUTH.user.displayName}
         // handleChange={handleChange()
-
         />
+        <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined" />
+
+      <FormControl className={classes.margin}>
+        <InputLabel
+        shrink htmlFor="bootstrap-input"
+        defaultValue={AUTH.user.displayName}
+        >
+          Bootstrap
+        </InputLabel>
+        <BootstrapInput defaultValue="react-bootstrap" id="bootstrap-input" />
+      </FormControl>
+
+      </form>
 
       {/* </div> */}
     </div>
