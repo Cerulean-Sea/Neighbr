d5f448e96975b0541e5ad7a98c629ca9e0ae55e5
Tyler Jones <tylerj.audio94@gmail.com>
2021-09-01 19:53:28 -0700

    Infinite Scroll fixed

--- client/src/components/Posts/PostList.js
+++ client/src/components/Posts/PostList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { Typography, Paper } from '@material-ui/core';
@@ -22,35 +22,59 @@ export default (props) => {
   const location = useLocation();
   const userId = AUTH?.user?.uid;
 
+  let pageLimit = 20;
+  const [loading, setLoading] = useState(true);
+  const observer = useRef();
+  const lastPostRef = useCallback((node) => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        console.log('visible');
+        actions.posts(dispatch, 1, pageLimit);
+        pageLimit += 10;
+      }
+    });
+    if (node) observer.current.observe(node);
+  }, [loading])
+
   useEffect(() => {
+    setLoading(true);
     if (location.pathname === '/') {
       actions.posts(dispatch);
+      setLoading(false);
     } else if (location.pathname === '/profile') {
       dispatch(actions.getPostsByUserId(userId));
+      setLoading(false);
     }
   }, []);
 
-  let pageLimit = 20;
-  if (location.pathname === '/' && localStorage.profile) {
-    window.addEventListener('scroll', (e) => {
-      console.log('bottom');
-      // const scrollY = window.scrollY;
-      // const innerHeight = window.innerHeight;
-      // const docHeight = document.documentElement.scrollHeight;
+  // if (location.pathname === '/' && localStorage.profile) {
+  //   window.addEventListener('scroll', (e) => {
+  //     console.log('bottom');
+  //     // const scrollY = window.scrollY;
+  //     // const innerHeight = window.innerHeight;
+  //     // const docHeight = document.documentElement.scrollHeight;
 
-      // if (scrollY + innerHeight >= docHeight && localStorage.profile) {
-      //   actions.posts(dispatch, 1, pageLimit);
-      //   pageLimit += 10;
-      // }
-      // e.stopImmediatePropagation();
+  //     // if (scrollY + innerHeight >= docHeight && localStorage.profile) {
+  //     //   actions.posts(dispatch, 1, pageLimit);
+  //     //   pageLimit += 10;
+  //     // }
+  //     // e.stopImmediatePropagation();
 
 
-    });
-  };
+  //   });
+  // };
 
   return (
     <>
-      {posts.map((p) => <div className="post-list-item" key={p._id}><Post post={p} /></div>)}
+      {posts.map((p, i) => {
+        if (posts.length === i + 1) {
+          return <div className="post-list-item" key={p._id} ref={lastPostRef}><Post post={p} /></div>
+        } else {
+          return <div className="post-list-item" key={p._id}><Post post={p} /></div>
+        }
+      })}
     </>
   );
 };
