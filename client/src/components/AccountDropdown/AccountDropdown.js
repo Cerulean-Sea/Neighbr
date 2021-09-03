69d2b72f8979f1ef430a51d4f78db27b702ef3df
Ben Bernardy <benbernardy@gmail.com>
2021-09-02 17:56:32 -0700

    ADDED: Mobile Layout. TODO: Popup Menu needs correct anchor in Mobile view

--- client/src/components/AccountDropdown/AccountDropdown.js
+++ client/src/components/AccountDropdown/AccountDropdown.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useHistory, useLocation } from 'react-router-dom';
-import {MenuItem, makeStyles, Menu, AppBar, Toolbar, Avatar, Box, Button, Hidden } from '@material-ui/core';
+import {
+  MenuItem, makeStyles, Menu, AppBar,
+  Toolbar, Avatar, Box, Button, Hidden,
+  CssBaseline, Fab } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
+import TagIcon from '@material-ui/icons/LocalOffer';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import ChatIcon from '@material-ui/icons/ForumOutlined';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const AccountDropdown = () => {
 
-  const mobileLayout = window.innerWidth > 600 ? false : true;
+
   const AUTH = useSelector(state => state.firebase);
   const dispatch = useDispatch();
   const history = useHistory();
@@ -45,8 +52,26 @@ const AccountDropdown = () => {
       backgroundColor: '#2A9D8F'
     }
   }));
+  const useMobileStyles = makeStyles((theme) => ({
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    grow: {
+      flexGrow: 3,
+    },
+    fabButton: {
+      position: 'absolute',
+      zIndex: 2000,
+      top: -30,
+      left: '80%',
+      right: 0,
+      margin: '0 auto',
+    },
+  }));
 
   const classes = useStyles();
+  const mobileClasses = useMobileStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -76,10 +101,10 @@ const AccountDropdown = () => {
   }, [location]);
 
   return (
-    <div>
-    <AppBar position="static"
-    className={classes.appbar}
-    >
+    <>
+      {/* APP BAR TOP ABOVE 600PX WIDTH */}
+    <Hidden xsDown>
+    <AppBar position="static" className={classes.appbar}>
         <Toolbar className={classes.accountButton}>
           <Box display="flex" flexGrow={1}>
             <Avatar src='./assets/logo.png' component={Link} to="/"/>
@@ -154,7 +179,66 @@ const AccountDropdown = () => {
           )}
       </Toolbar>
     </AppBar>
-    </div>
+    </Hidden>
+    {/* MOBILE APP BAR LAYOUT BELOW */}
+    <Hidden smUp>
+      <CssBaseline />
+      <AppBar position="fixed" color="primary" className={mobileClasses.appBar}>
+        <Toolbar>
+          {!AUTH &&
+            <Box display="flex" flexGrow={1}>
+              <Avatar src='./assets/logo.png' component={Link} to="/"/>
+            </Box>}
+          {!AUTH && (
+            <Button component={Link} className={classes.btn} to="/login" variant="contained" color="default">Login</Button>
+          )}
+        {AUTH && <>
+          <Fab
+            color="secondary"
+            aria-label="add"
+            className={mobileClasses.fabButton}
+            onClick={() => dispatch({ type: 'SHOW_POST'})}>
+            <AddIcon />
+          </Fab>
+          {/* <IconButton
+                className={classes.menu}
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              > */}
+              <div style={{flexGrow:1}}>
+                {AUTH.user.photoURL ?
+                  <Avatar
+                    src={AUTH.user.photoURL}
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"/> :
+                  <AccountCircle/>}
+                </div>
+              {/* </IconButton> */}
+          <div className={mobileClasses.grow}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={() => dispatch({ type: 'SHOW_FILTER'})}>
+            <TagIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            edge="start"
+            component={Link} to='/chat'>
+            <ChatIcon />
+          </IconButton>
+          </div>
+        </>}
+        </Toolbar>
+      </AppBar>
+    </Hidden>
+    </>
   )
 }
 
